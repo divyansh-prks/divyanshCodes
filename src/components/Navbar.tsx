56bbec3d6d0909f54da899ded3010c9a7b0b3e29
divyansh-prks <divyansh2375@gmail.com>
2025-08-27 14:55:57 +0530

    added navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -12,17 +12,17 @@ const navItems = [
 export default function Navbar({ activeSection, setActiveSection }: { activeSection: string, setActiveSection: (key: string) => void }) {
   return (
     <nav className="">
-      <div className="w-fit mx-auto px-8 py-3">
+      <div className="w-[750px] bg-black rounded-bl-2xl rounded-br-2xl text-white mx-auto px-8 py-3">
         <div className="flex justify-center">
           <div className="flex space-x-6">
             {navItems.map(item => (
               <button
                 key={item.key}
                 onClick={() => setActiveSection(item.key)}
-                className={`text-sm transition-colors flex items-center space-x-1 ${
+                className={`text-1xl mt-3 transition-colors flex items-center space-x-1 ${
                   activeSection === item.key
-                    ? 'text-gray-900 font-medium'
-                    : 'text-gray-700 hover:text-gray-900'
+                    ? 'text-yellow-400 font-medium'
+                    : 'text-white hover:text-yellow-200'
                 }`}
               >
                 {item.icon && item.icon}
